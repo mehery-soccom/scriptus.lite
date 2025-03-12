c48cd6558926052bb96157344dd27e4b6433cb63
meerdaksh <mehery.soccom@gmail.com>
2025-03-12 14:12:39 +0530

    adapet

--- app-scriptus/workers/InboundQueue.js
+++ app-scriptus/workers/InboundQueue.js
@@ -1,6 +1,6 @@
 import { Job } from "@bootloader/core/decorators";
 import { ChatBox } from "./../../@core/scriptus";
-import { XMSAdapeter, LocalAdapeter } from "./../../@core/scriptus/adapters";
+import { XMSAdapter, LocalAdapter } from "./../../@core/scriptus/adapters";
 
 ChatBox.load({
   dir: "../",
@@ -13,9 +13,13 @@ export default class InboundQueue {
   }
 
   async execute({ task, queue }) {
+    console.log("InboundQueue > execute", { task, queue });
+
     const contact_id = queue;
     new ChatBox({
-      adapter: new LocalAdapeter({ message: task, contact_id }),
+      adapter: task.meta
+        ? new XMSAdapter({ message: task, contact_id })
+        : new LocalAdapter({ message: task, contact_id }),
     }).execute();
   }
 }
