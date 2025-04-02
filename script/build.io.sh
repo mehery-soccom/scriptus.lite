3894badd2697ebcdd4b335ec4720578004b6854b
meerdaksh <mehery.soccom@gmail.com>
2025-04-02 16:38:38 +0530

    added build script

--- script/build.io.sh
+++ script/build.io.sh
@@ -7,4 +7,4 @@ git pull --rebase origin build-xyz-${module}
 git checkout build-io-${module}
 git pull --rebase origin build-io-${module}
 git merge build-xyz-${module}
-git push origin build-xyz-${module}
+git push origin build-io-${module}
