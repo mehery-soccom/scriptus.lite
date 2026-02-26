94a3bb67a8c19773e54f44388e1cd6a85207c776
meerdaksh <meerdaksh@mehery.com>
2026-02-26 17:20:20 +0530

    added dummy bot

--- app-scriptus/bots/secret_santa.js
+++ app-scriptus/bots/secret_santa.js
@@ -6,11 +6,11 @@ function onSessionRouted() {
   console.log("=======================TEST====onSessionRouted===");
 }
 
-async function  onMessageReceive() {
+async function onMessageReceive() {
   //console.log("Recieved Message:", $.inbound);
-  let resp = await $.reply({
-      text: `ResponseOfSanta(${$.inbound.getText()})`,
-      options: { buttons: ["Enter Circle","Create Circle"]}
-  })
+  let testresp = await $.calc(200).add(50).multiply(4).substract(500);
+  console.log("testresp", testresp);
+
+  let resp = await $.reply(`Response(${$.inbound.getText()})`);
   console.log("Response:", resp);
 }
